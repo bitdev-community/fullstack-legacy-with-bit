2ba61364f3c44694eb2e4c00542f8b85dba30d9f
ashanfernando <ashan256@gmail.com>
2023-03-29 15:21:50 +0530

    Adding support for API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+const router = express.Router();
 
 const app = express();
 app.use(cors());
@@ -7,6 +8,9 @@ app.use(cors());
 app.get('/hello', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+// Add the prefix to all API routes
+app.use('/api', router);
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
