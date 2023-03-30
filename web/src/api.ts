5fc1d0975c4395edd800c24dbe984894bf846462
ashanfernando <ashan256@gmail.com>
2023-03-30 17:00:57 +0530

    Update Axios to use HTTP

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
+import http from 'http';
 
 const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_HELLO_API_HOST,
+  httpAgent: new http.Agent({ keepAlive: true }) // Remove this, after API is hosted in HTTPS URL
 });
 
 export default api;
