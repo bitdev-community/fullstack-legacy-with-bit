be3aad028d7e3fdb9ab1fdc8fe1c2a6e2acef45e
ashanfernando <ashan256@gmail.com>
2023-03-30 17:11:33 +0530

    Netlify fix port

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,9 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
-import http from 'http';
 
 const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_HELLO_API_HOST,
-  httpAgent: new http.Agent({ keepAlive: true }) // Remove this, after API is hosted in HTTPS URL
 });
 
 export default api;
