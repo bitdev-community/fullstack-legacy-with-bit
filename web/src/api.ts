d795d323e0c7c23373b9eee78363db4f7f557fdc
ashanfernando <ashan256@gmail.com>
2023-03-30 15:44:06 +0530

    Change API Host

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_HELLO_API_URL,
+  baseURL: process.env.REACT_APP_HELLO_API_HOST,
 });
 
 export default api;
