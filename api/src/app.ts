3c67403d0177fb51a2938b5317b3a17fde54d85c
ashanfernando <ashan256@gmail.com>
2023-04-02 17:08:13 +0530

    Making the port dynamic

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import helloRouter from './routes/hello';
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
 
 app.use('/api', helloRouter);
 
-app.listen(4000, () => {
-  console.log('Server listening on port 4000');
+app.listen(port, () => {
+  console.log(`Server listening on port $(port)`);
 });
 
 export default app;
