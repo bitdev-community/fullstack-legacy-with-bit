66a779f7a73fbf3ac18705d196bca34c25536636
ashanfernando <ashan256@gmail.com>
2023-04-02 11:17:47 +0530

    Update API project structure

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,11 @@
-import express, { Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
 import cors from 'cors';
+import helloRouter from './routes/hello/hello';
 
 const app = express();
 app.use(cors());
 
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use('/api', helloRouter);
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
