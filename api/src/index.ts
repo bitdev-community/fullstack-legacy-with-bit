c01afb7081a780f4292ba29649cbc889e31a1de1
ashanfernando <ashan256@gmail.com>
2023-04-02 12:56:26 +0530

    Change the api structure

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,2 @@
-import express from 'express';
-import cors from 'cors';
-import helloRouter from './routes/hello/hello';
-
-const app = express();
-app.use(cors());
-
-app.use('/api', helloRouter);
-
-app.listen(4000, () => {
-  console.log('Server listening on port 4000');
-});
+import app from "./app";
+export default app;
