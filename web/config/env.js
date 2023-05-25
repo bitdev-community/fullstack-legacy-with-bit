ba488fa8ecc13320c618cd5a2d9cd6ff3f670daa
ashanfernando <ashan256@gmail.com>
2023-05-25 13:28:24 +0000

    commenting dotenvfiles

--- web/config/env.js
+++ web/config/env.js
@@ -30,15 +30,15 @@ const dotenvFiles = [
 // that have already been set.  Variable expansion is supported in .env files.
 // https://github.com/motdotla/dotenv
 // https://github.com/motdotla/dotenv-expand
-dotenvFiles.forEach(dotenvFile => {
-  if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand')(
-      require('dotenv').config({
-        path: dotenvFile,
-      })
-    );
-  }
-});
+// dotenvFiles.forEach(dotenvFile => {
+//   if (fs.existsSync(dotenvFile)) {
+//     require('dotenv-expand')(
+//       require('dotenv').config({
+//         path: dotenvFile,
+//       })
+//     );
+//   }
+// });
 
 // We support resolving modules according to `NODE_PATH`.
 // This lets you use absolute paths in imports inside large monorepos:
